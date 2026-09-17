6318c5bab6cfa0cbc8130cf0e974187c7b9e072c
telekikrisz <kallai.krisztian@telekimezotur.hu>
2026-09-17 12:40:25 +0200

    alapadatok

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string Utasnev="Márton Károly";
+int Tavolsag=14;
+bool Ejszaka=true;
+int Alapdíj = 1100;
+int kmDíj = 440;
+double EjszakaSzorzo = 1.2;
+
+
+
+
